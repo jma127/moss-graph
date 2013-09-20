8ecabe068c522176fee77f545387a54e4467ae5f
Jerry Ma <jerryma@nospam>
2013-09-20 16:49:34 -0400

    Component analysis

--- moss-graph.py
+++ moss-graph.py
@@ -49,6 +49,15 @@ class MossParser(HTMLParser):
         if self.on and tag.lower() == 'a':
             self.on = False
 
+def get_component(node, matrix, used):
+    if node in used:
+        return []
+    used.add(node)
+    comp = [node]
+    for node2 in matrix[node]:
+        comp += get_component(node2, matrix, used)
+    return comp
+
 def main():
     if len(sys.argv) < 3: # We should probably use argparse, but w/e
         print '\n'.join([
@@ -62,27 +71,48 @@ def main():
     parser = MossParser()
     parser.feed(content)
 
+    print 'Summary for MOSS report', sys.argv[1]
+    print
+
     print 'Student frequencies'
     sorted_frequencies = list(parser.frequencies.items())
     sorted_frequencies.sort(key=lambda elem: elem[1], reverse=True)
     for key, val in sorted_frequencies:
-        print '"{}",{}'.format(key, val)
+        print '{:>41s}{:>9d}'.format(key, val)
+
+    matrix = {}
+    for edgeid, edge in parser.edges.iteritems():
+        a, b = edge.nodes[0], edge.nodes[1]
+        matrix.setdefault(a, {}).setdefault(b, edge.weight / 2.0)
+        matrix.setdefault(b, {}).setdefault(a, edge.weight / 2.0)
+    nodes = list(matrix.keys())
 
     with open(sys.argv[2], 'wb') as outfile:
-        matrix = {}
-        for edgeid, edge in parser.edges.iteritems():
-            a, b = edge.nodes[0], edge.nodes[1]
-            matrix.setdefault(a, {}).setdefault(b, edge.weight / 2.0)
-            matrix.setdefault(b, {}).setdefault(a, edge.weight / 2.0)
-        nodes = list(matrix.keys())
         for node in nodes:
-            outfile.write(',"' + node + '"')
+            outfile.write(';"' + node + '"')
         outfile.write('\n')
         for node1 in nodes:
             outfile.write('"' + node1 + '"')
             for node2 in nodes:
-                outfile.write(',{:.2f}'.format(matrix[node1].get(node2, 0.0)))
+                outfile.write(';{}'.format(matrix[node1].get(node2, 0)))
             outfile.write('\n')
+
+    comps = []
+    used = set()
+    for node in nodes:
+        comp = get_component(node, matrix, used)
+        if comp:
+            comps.append(comp)
+    comps.sort(key=lambda comp:len(comp), reverse=True)
+
+    print
+    print 'Components'
+    for comp in comps:
+        sorted_comp = [(node, sum(matrix[node][node2] for node2 in matrix[node]) / (len(comp) - 1)) for node in comp]
+        sorted_comp.sort(key=lambda node: node[1], reverse=True)
+        print 'Size {:>4d} ========================================'.format(len(sorted_comp))
+        for node, deg in sorted_comp:
+            print '{:>41s}{:>9.2f}'.format(node, deg)
 
 if __name__ == '__main__':
     main()
